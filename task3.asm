d60b67c7b714ad7083ff0838a950ee5f3b36413a
Alex <98720097+MmmCheesy@users.noreply.github.com>
2022-11-22 23:04:11 -0800

    Add files via upload

---
+++ task3.asm
@@ -0,0 +1,57 @@
+#Assignment: exponenets
+
+#Nam: Alexander Eckert
+#Date: 11/22/22
+
+.data
+
+	msg: .asciiz "This programs computes 'x' to the power of 'y'.\n\n\nEnter a value for 'x': "
+	msgCont: .asciiz "Enter a value for 'y': "
+	msg2: .asciiz "'x' to the power of 'y' is: "
+	
+.text
+
+	main:
+		li $v0, 4
+		la $a0, msg
+		syscall
+		
+		li $v0, 5
+		syscall
+		move $s0, $v0
+		
+		li $v0, 4
+		la $a0, msgCont
+		syscall
+		
+		li $v0, 5
+		syscall
+		move $s1, $v0
+	
+		#loop counter
+		li $t0, 2
+		
+		la $t1, ($s0)
+		
+		j loop
+		
+	loop:
+		addi $t0, $t0, 1
+		
+		mul $s0, $s0, $t1
+		
+		ble $t0, $s1, loop
+		
+		j exit
+	
+	exit:
+		li $v0, 4
+		la $a0, msg2
+		syscall
+		
+		li $v0, 1
+		move $a0, $s0
+		syscall
+		
+		li $v0, 10
+		syscall
